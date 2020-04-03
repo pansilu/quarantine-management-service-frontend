d130b85028717ed366cbf0400a5584afd35dc02a
yohan kulasinghe <yohankulasinghe@gmail.com>
2020-04-04 01:36:47 +0530

    Added validations and model bindings

--- src/app/quarantine/components/add-edit-person/add-edit-person.component.ts
+++ src/app/quarantine/components/add-edit-person/add-edit-person.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { QuarantineService } from 'src/app/Service/quarantine.service';
 import { QuarantinePersonEditModel } from '../../models/quarantine-person-edit.model';
 import { ToastService } from 'src/app/Service/toast.service';
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgIfContext } from '@angular/common';
 
 @Component({
   selector: 'app-add-edit-person',
@@ -10,84 +13,90 @@ import { ToastService } from 'src/app/Service/toast.service';
 })
 export class AddEditPersonComponent implements OnInit {
 
-  isSuspected: boolean;
-  isInfected: boolean;
+  form: FormGroup;
+  person: QuarantinePersonEditModel;
+
+  submitted: boolean;
+  edit: boolean;
+
+  errGramaSewaDivision: boolean;
 
   locationData: any
+
   divisions = [];
   policeStations = [];
   gramaSewaDivisions = [];
-  gsd = []
 
   countries = [];
-  countrynames = []
-
-  q_person: any;
-
-  qp_division: string
-  qp_policeStation: string
-  qp_gramaSewaDivisionName: string
-  qp_gramaSewaDivisionId: number
-  qp_fileNo: string
-
-  qp_nic: string
-  qp_passportNo: string
-  qp_name: string
-  qp_address: string
-  qp_age: number
-  qp_reportedDate: string
-  qp_mobile: string
-  qp_phone: string
-  qp_appEnable: boolean
-
-  qp_otherInfo: string
-
-  qp_arrivalDate: string
-  qp_countryName: string
-  qp_countryId: string
-  qp_informedDate: string
-
   qp_inspectorIds: []
 
-  gr_name: string
-  gr_mobile: string
-  gr_nic: string
-  gr_passportNo: string
+  id: any;
+  q_person: any;
 
-
-  constructor(private _quarantineService: QuarantineService,private _toast: ToastService) {
+  constructor(private _quarantineService: QuarantineService, private _toast: ToastService, private _formBuilder: FormBuilder, private _router: Router) {
     this.q_person = new QuarantinePersonEditModel()
   }
 
 
   ngOnInit() {
-    this.isSuspected = false;
-    this.isInfected = false;
+
+    this.edit = false
+
     this.getCountries();
     this.getLocation();
 
-    // Hardcode values for testing 
+    if (this.edit) {
+      this.id = 10
+      // get user form back end
+    } else {
+      this.id = null;
+      this.person = new QuarantinePersonEditModel();
+      this.createForm();
+    }
 
-    /* this.q_person.address = {id:null, line:"ABDERd"}
-     this.q_person.age = 45
-     this.q_person.appEnable = true
-     this.q_person.id = null
-     this.q_person.countryId = 12458
-     this.q_person.fileNo = "ASD123"
-     this.q_person.gramaSewaDivisionId = 123
-     this.q_person.guardianDetails = null
-     this.q_person.informedDate = "2020-02-12"
-     this.q_person.inspectorIds = null
-     this.q_person.name = "nimal"
-     this.q_person.mobile = "0174456989"
-     this.q_person.phone = "0.21452687"
-     this.q_person.reportedDate = null
-     this.q_person.nic = "123456788v"
-     this.q_person.passportNo = "NB1234Q234"
-     this.q_person.secret = "hi"
- 
-     console.log(this.q_person)
-     this.setQuarantinePerson()*/
+    this.createForm();
+
+  }
+
+  createForm() {
+    const model = {
+      qp_division: new FormControl(this.person.division),
+      qp_policeStation: new FormControl(this.person.policeStation),
+      qp_gramaSewaDivisionId: new FormControl(this.person.gramaSewaDivisionId, Validators.required),
+      qp_fileNo: new FormControl(this.person.fileNo),
+
+      qp_nic: new FormControl(this.person.nic, Validators.required),
+      qp_passportNo: new FormControl(this.person.passportNo),
+      qp_name: new FormControl(this.person.name, Validators.required),
+      qp_address: new FormControl(this.person.address.line, Validators.required),
+      qp_age: new FormControl(this.person.age, Validators.required),
+      qp_reportedDate: new FormControl(this.person.reportedDate, Validators.required),
+
+      qp_mobile: new FormControl(this.person.mobile, Validators.required),
+      qp_phone: new FormControl(this.person.phone),
+      qp_appEnable: new FormControl(this.person.appEnable),
+
+      qp_otherFacts: new FormControl(this.person.otherFacts),
+
+      qp_arrivalDate: new FormControl(this.person.arrivalDate),
+      qp_countryId: new FormControl(this.person.countryId),
+      qp_informedDate: new FormControl(this.person.informedDate),
+
+      // Officer Data handled seperately
+
+      qp_admittedDate: new FormControl(this.person.admittedDate),
+      qp_admitHosId: new FormControl(this.person.admitHosId),
+      qp_dischargedDate: new FormControl(this.person.dischargedDate),
+
+      qp_confirmedDate: new FormControl(this.person.confirmedDate),
+      qp_confirmedHosId: new FormControl(this.person.confirmedHosId),
+
+      gr_name: new FormControl(this.person.guardianDetails.name),
+      gr_nic: new FormControl(this.person.guardianDetails.nic),
+      gr_passportNo: new FormControl(this.person.guardianDetails.passportNo),
+      gr_mobile: new FormControl(this.person.guardianDetails.mobile),
+    };
+    this.form = this._formBuilder.group(model);
 
   }
 
@@ -95,7 +104,6 @@ export class AddEditPersonComponent implements OnInit {
     this._quarantineService.getCountries((d) => {
       d.forEach(c => {
         this.countries.push(c);
-        this.countrynames.push(c.name);
       })
     }, e => {
       console.log(e);
@@ -103,11 +111,7 @@ export class AddEditPersonComponent implements OnInit {
   }
 
   counrtySelected() {
-    this.countries.forEach(e => {
-      if (e.name == this.qp_countryName) {
-        this.qp_countryId = e.id
-      }
-    })
+
   }
 
   getLocation() {
@@ -131,7 +135,7 @@ export class AddEditPersonComponent implements OnInit {
 
   filterPoliceStation() {
     this.locationData.forEach(element => {
-      if (element.name == this.qp_division) {
+      if (element.name == this.form.value.qp_division) {
         const ps = element.stations
         ps.forEach(e => {
           this.policeStations.push(e.name)
@@ -149,10 +153,10 @@ export class AddEditPersonComponent implements OnInit {
     this.locationData.forEach(element => {
       const ps = element.stations
       ps.forEach(e => {
-        if (e.name == this.qp_policeStation) {
-          this.gsd = e.gramaSewaDivisions
-          this.gsd.forEach(e => {
-            this.gramaSewaDivisions.push(e.name)
+        if (e.name == this.form.value.qp_policeStation) {
+          const gsd = e.gramaSewaDivisions
+          gsd.forEach(e => {
+            this.gramaSewaDivisions.push(e)
           })
         }
       })
@@ -161,17 +165,75 @@ export class AddEditPersonComponent implements OnInit {
   }
 
   gramaSewaDivisionSelected() {
-    this.gsd.forEach(e => {
-      if (e.name == this.qp_gramaSewaDivisionName) {
-        this.qp_gramaSewaDivisionId = e.id
+    if (this.form.value.gramaSewaDivisionId == NaN) {
+      this.errGramaSewaDivision = true
+    }
+  }
+
+
+  addNewPerson() {
+    this.submitted = true;
+
+    if (this.form.valid) {
+
+      // ID use to seperate add or edit
+      this.q_person.id = this.id
+
+      this.q_person.gramaSewaDivisionId = +this.form.value.qp_gramaSewaDivisionId
+      this.q_person.fileNo = this.form.value.qp_fileNo
+
+      this.q_person.nic = this.form.value.qp_nic
+      this.q_person.passportNo = this.form.value.qp_passportNo
+      this.q_person.name = this.form.value.qp_name
+      this.q_person.address = {
+        id: null,
+        line: this.form.value.qp_address
       }
-    })
+      this.q_person.age = this.form.value.mainForm
+      this.q_person.reportedDate = this.form.value.qp_reportedDate
+
+      this.q_person.mobile = this.form.value.qp_mobile
+      this.q_person.phone = this.form.value.qp_phone
+      this.q_person.appEnable = this.form.value.qp_appEnable
+
+      this.q_person.otherFacts = this.form.value.qp_otherFacts
+
+      this.q_person.arrivalDate = this.form.value.qp_arrivalDate
+      this.q_person.countryId = this.form.value.qp_countryId
+      this.q_person.informedDate = this.form.value.qp_informedDate
+
+      this.q_person.inspectorIds = null
+
+      this.q_person.admittedDate = this.form.value.qp_admittedDate
+      this.q_person.admitHosId = this.form.value.qp_admitHosId
+      this.q_person.dischargedDate = this.form.value.qp_dischargedDate
+
+      this.q_person.confirmedDate = this.form.value.qp_confirmedDate
+      this.q_person.confirmedHosId = this.form.value.qp_confirmedHosId
+
+      this.q_person.guardianDetails = {
+        id: null,
+        //name
+        mobile: this.form.value.gr_mobile,
+        nic: this.form.value.gr_nic,
+        passportNo: this.form.value.gr_passportNo
+      }
+
+      this.q_person.secret = "hi"
+
+     // console.log(this.q_person)
+      this.setQuarantinePerson()
+
+    } else {
+      this._toast.error("Error", "Please Fill Required Fields")
+    }
   }
 
   setQuarantinePerson() {
     this._quarantineService.setQuarantinePerson((d) => {
-      this._toast.success("Success","New person added");
       console.log(d);
+      this._toast.success("Success", "Person saved");
+      this.close_add_new();
     }, e => {
       console.log(e);
     }, this.q_person);
@@ -179,113 +241,14 @@ export class AddEditPersonComponent implements OnInit {
     this.resetForm();
   }
 
-  checkIfSuspected(event) {
-    if (event.target.checked) {
-      this.isSuspected = true;
-    } else {
-      this.isSuspected = false;
-    }
+  close_add_new() {
+    document.getElementById('addNew').style.visibility = 'hidden';
+    document.getElementById('addNew').style.opacity = '0';
+    this.resetForm();
   }
 
-  checkIfInfected(event) {
-    if (event.target.checked) {
-      this.isInfected = true;
-    } else {
-      this.isInfected = false;
-    }
-  }
-
-  //functions for validation purpose - not yet implement
-
-  /*fillFileNo(qp_fileNo) {
-    this.qp_fileNo = qp_fileNo
-  }
-
-  fillQPNIC(qp_nic) {
-    this.qp_nic = qp_nic
-  }
-
-  fillQPPasspoerNo(qp_passportNo) {
-    this.qp_passportNo = qp_passportNo
-  }
-
-  fiiQPName(qp_name) {
-    this.qp_name = qp_name
-  }
-
-  fillQPAddres(qp_address) {
-    this.qp_address = qp_address
-  }
-
-  fillQPAge(qp_age) {
-    this.qp_age = +qp_age
-  }
-
-  fillQPReportDate() {
-    console.log(this.qp_reportedDate)
-  }
-
-  fillQPMobile(qp_mobile) {
-    this.qp_mobile = qp_mobile
-  }
-
-  fillQPPhone(qp_phone) {
-    this.qp_phone = qp_phone
-  }
-
-  fillQPInformedDate() {
-    console.log(this.qp_informedDate)
-  }*/
-
-  canUseApp(event) {
-    this.qp_appEnable = event.checked
-  }
-
-  addNewPerson() {
-    this.q_person.address = { id: null, line: this.qp_address }
-    this.q_person.age = this.qp_age
-    this.q_person.appEnable = this.qp_appEnable
-    this.q_person.id = null
-    //  this.q_person.arrivalDate = "2020-02-10"
-    this.q_person.countryId = this.qp_countryId
-    this.q_person.fileNo = this.qp_fileNo
-    this.q_person.gramaSewaDivisionId = this.qp_gramaSewaDivisionId
-    this.q_person.guardianDetails = null
-    this.q_person.informedDate = this.qp_informedDate
-    this.q_person.inspectorIds = null
-    this.q_person.name = this.qp_name
-    this.q_person.mobile = this.qp_mobile
-    this.q_person.phone = this.qp_phone
-    this.q_person.reportedDate = this.qp_reportedDate
-    this.q_person.nic = this.qp_nic
-    this.q_person.passportNo = this.qp_passportNo
-    this.q_person.secret = "hi"
-
-    console.log(this.q_person)
-    this.setQuarantinePerson()
-  }
-
-  resetForm(){
-    // this.qp_division = "";
-    // this.qp_policeStation = "";
-    // this.qp_gramaSewaDivisionName = "";
-    // this.qp_gramaSewaDivisionId = null;
-    this.qp_fileNo = null;
-
-    this.qp_nic = "";
-    this.qp_passportNo = "";
-    this.qp_name = "";
-    this.qp_address = "";
-    this.qp_age = null
-    this.qp_reportedDate = "";
-    this.qp_mobile  = "";
-    this.qp_phone = "";
-    this.qp_appEnable = null
-
-    this.qp_arrivalDate = "";
-    this.qp_countryName = "";
-    this.qp_countryId = "";
-    this.qp_informedDate = "";
+  resetForm() {
+    this.form.reset()
   }
 
 }
--- src/app/quarantine/models/quarantine-person-edit.model.ts
+++ src/app/quarantine/models/quarantine-person-edit.model.ts
@@ -1,5 +1,8 @@
 export class QuarantinePersonEditModel {
-    public address:address[]
+    public division:string
+    public policeStation:string
+
+    public address:address
     public age:number
     public appEnable:boolean
     public id:number
@@ -17,15 +20,28 @@ export class QuarantinePersonEditModel {
     public nic:string
     public passportNo:string
     public secret: string
+    public otherFacts:string
+    public admittedDate:string
+    public admitHosId:number
+    public dischargedDate:string
+    public confirmedDate:string
+    public confirmedHosId:number
+
+
+    constructor(){
+        this.address = new address()
+        this.guardianDetails = new guardianDetails()
+    }
 }
 
-interface address{
+export class address{
     id:number
     line:string
 }
 
-interface guardianDetails{
+export class guardianDetails{
     id:number
+    name: string
     mobile:string
     nic:string
     passportNo:string
