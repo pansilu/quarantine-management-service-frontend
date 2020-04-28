1b0bddc09abe17ed21095aaea6947e69a3cdb69f
dsdissnayaka <danushka@bourketechnologies.com>
2020-04-28 16:07:34 +0530

    -- add q person done

--- src/app/quarantine/models/quarantine-person-view.model.ts
+++ src/app/quarantine/models/quarantine-person-view.model.ts
@@ -1,22 +1,17 @@
+import { AddressModel } from './address.model'
+import { NameIdModel } from 'src/app/shared/models/name-id.model'
+
 export class QuarantinePersonViewModel {
-    public age: number
-    public completed: boolean
-    public fileNo: string
-    public id: number
-    public informedAuthority: boolean
-    public informedDate: string
-    public lastUpdateDate: string
-    public mobile: string
-    public name: string
-    public needToRemind: boolean
-    public nic: string
-    public noticeAttachDate: string
-    public passportNo: string
-    public patient: true
-    public phone: string
-    public remainingDays: number
-    public reportDate: string
-    public totalPoints: 0
+    address: AddressModel
+    age: number
+    arrivalDate: string
+    arrivedCountry: NameIdModel
+    id: number
+    mobile: string
+    name: string
+    nic: string
+    passportNo: string
+    phone: string
     constructor() {
 
     }
